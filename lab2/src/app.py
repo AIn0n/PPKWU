616040d38f3eb7416039197cac07da0520c03868
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-10-27 12:53:18 +0200

    service is working

--- lab2/src/app.py
+++ lab2/src/app.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, request
+import time
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<p>Hello, World!</p>"
+    params = request.args.to_dict()
+    if params["cmd"] == "time":
+        return str(time.strftime("%H:%M:%S"))
+    if params["cmd"] == "rev":
+        return reversed(params["rev"])
+    return "Hello world!"
