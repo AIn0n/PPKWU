7b2fe8ee627da44d921f13fd3cb62a360417d224
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-11-10 12:40:14 +0100

    Added lowercase functionality

--- lab3/src/app.py
+++ lab3/src/app.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 from flask import Flask, request
-import time
+import json
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     params = request.args.to_dict()
-    if "cmd" in params and params["cmd"] == "time":
-        return str(time.strftime("%H:%M:%S"))
-    if "cmd" in params and "str" in params and params["cmd"] == "rev":
-        return reversed(params["str"])
-    return "Hello world!"
+    if "str" in params:
+        string = params["str"]
+        return json.dumps({"lowercase": sum(1 for char in string if char.islower())})
+    return "no str parameter found"
