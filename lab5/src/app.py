81935efe8fb7dccf675f50e90e2cbcb19b8593aa
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-12-08 12:57:29 +0100

    Added string part of lab

--- lab5/src/app.py
+++ lab5/src/app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
 def hello():
+    result = {}
     params = request.get_json()
-    print(params)
+    if 'str' in params:
+        string = params['str']
+        result['lowercase'] = sum(map(str.islower, string))
+        result['digits'] = sum(map(str.isdigit, string))
+        result['uppercase'] = sum(map(str.isupper, string))
+        result['special'] = sum(not char.isalpha() and not char.isdigit() for char in string)
     return jsonify(params)
