c0e316b4647a09f37b18bb41aa5114c92bec2d8e
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-12-22 13:25:33 +0100

    Still working with numberrs

--- lab6/src/app.py
+++ lab6/src/app.py
@@ -11,17 +11,18 @@ def hello():
     params = et.fromstring(xml_data)
     string =  params.find("str")
     if string != None:
-        string = string.text
         result['lowercase'] = sum(map(str.islower, string))
         result['digits']    = sum(map(str.isdigit, string))
         result['uppercase'] = sum(map(str.isupper, string))
         result['special']   = sum(not char.isalpha() and not char.isdigit() for char in string)
 
-#    if 'num1' in params and 'num2' in params:
-#        a, b = params['num1'], params['num2']
-#        result['sum'] = a + b
-#        result['sub'] = a - b
-#        result['mul'] = a * b
-#        result['div'] = a // b
-#        result['mod'] = a % b
+    num1 = params.find('num1')
+    num2 = params.find('num2')
+    if 'num1' is not None and 'num2' is not None:
+        a, b = int(num1.text), int(num2.text)
+        result['sum'] = a + b
+        result['sub'] = a - b
+        result['mul'] = a * b
+        result['div'] = a // b
+        result['mod'] = a % b
     return jsonify(result)
